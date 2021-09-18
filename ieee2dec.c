ef5793300ca2112a0b128fa3ad0ea746ba9043b1
LeDaffy <57289615+LeDaffy@users.noreply.github.com>
2021-09-18 11:49:31 -0400

    fixed change

--- ieee2dec.c
+++ ieee2dec.c
@@ -210,7 +210,6 @@ bitString[0], expString, manString);
 
 	double finalAnswer = (double)wholeNumber + fracNumber;
 	if (isNeg) finalAnswer *= -1;
-<<<<<<< HEAD
 	if (!scientificNotation){
 		printf("\nConverted Output:\n%.24lf\n", finalAnswer);
 	} else {
@@ -218,8 +217,5 @@ bitString[0], expString, manString);
 	}
 	
 	
-=======
-	printf("\nConverted Output:\n%.24lf\n", finalAnswer);
->>>>>>> 2c5d1adffa25a822e70e933579b57f47dad6a676
 	return 0;
 }
